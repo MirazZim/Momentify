2c89580736e4236312100129dbf621b7d3233a77
MirazZim <miraz.zim.38@gmail.com>
2025-04-22 23:20:48 +0600

    Added user signup with Recoil state and localStorage - Created userAtom to store the user data in Recoil state. - Updated routes to show HomePage if the user is logged in, otherwise redirect to AuthPage. - Save user data to localStorage after successful signup and update the Recoil state. - Show toast messages for success and error during the signup process. - Now The signUP is perfectly working it is connected with database and backend completely

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,20 @@
 
 import { Container} from "@chakra-ui/react"
-import { Route, Routes } from "react-router-dom"
+import { Navigate, Route, Routes } from "react-router-dom"
 import UserPage from "./pages/UserPage"
 import PostPage from "./pages/PostPage"
 import Header from "./components/Header"
 import HomePage from "./pages/HomePage"
 import AuthPage from "./pages/AuthPage"
+import userAtom from "../atoms/userAtom.js"
+import { useRecoilValue } from "recoil"
+
 
 
 function App() {
+
+  const user = useRecoilValue(userAtom);
+
  
 
   return (
@@ -16,10 +22,10 @@ function App() {
       <Container maxW="620px" >
         <Header />
          <Routes>
-          <Route path="/" element={<HomePage />} />
+         <Route path='/' element={user ? <HomePage /> : <Navigate to='/auth' />} />
 
 
-          <Route path="/auth" element={<AuthPage />} />
+         <Route path='/auth' element={!user ? <AuthPage /> : <Navigate to='/' />} />
 
 
           <Route path="/:username" element={<UserPage />} />
--- frontend/src/components/SignUpCard.jsx
+++ frontend/src/components/SignUpCard.jsx
@@ -15,28 +15,93 @@ import {
     Text,
     useColorModeValue,
     Link,
+    useToast,
 } from '@chakra-ui/react'
 import { useState } from 'react'
 import { ViewIcon, ViewOffIcon } from '@chakra-ui/icons'
 import { useSetRecoilState } from 'recoil'
 import authScreenAtom from '../../atoms/authAtom.js'
+import userAtom from '../../atoms/userAtom.js'
 
 const SignUp = () => {
     const [showPassword, setShowPassword] = useState(false)
     const setAuthScreen = useSetRecoilState(authScreenAtom);
 
+    const [inputs, setInputs] = useState({
+        name: "",
+        username: "",
+        email: "",
+        password: "",
+    });
+
+    const toast = useToast();  // Corrected hook to `toast`
+
+    const setUser = useSetRecoilState(userAtom);
+
+    
+
+    const handleSignup = async () => {
+        console.log("handleSignup", inputs)
+
+        try {
+            const res = await fetch("/api/users/signup", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify(inputs),
+            });
+            const data = await res.json();
+
+            if (data.message) {
+                toast({
+                    title: "Error",
+                    description: data.message,
+                    status: "error",
+                    duration: 5000,
+                    isClosable: true,
+                });
+                return;
+            }
+
+            // Assuming you want to display a success message as well
+            toast({
+                title: "Sign Up Successful",
+                description: "You have successfully signed up.",
+                status: "success",
+                duration: 5000,
+                isClosable: true,
+            });
+
+            localStorage.setItem("user-threads", JSON.stringify(data));
+            setUser(data);
+			
+
+        } catch (error) {
+            toast({
+                title: "Error",
+                description: "An unexpected error occurred.",
+                status: "error",
+                duration: 5000,
+                isClosable: true,
+            });
+            console.log("error in handleSignup", error)
+        }
+    }
+
+
     return (
         <Flex
-           
+
             align={'center'}
             justify={'center'}
-           >
+        >
             <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6}>
                 <Stack align={'center'}>
                     <Heading fontSize={'4xl'} textAlign={'center'}>
                         Sign up
                     </Heading>
-                    
+
                 </Stack>
                 <Box
                     rounded={'lg'}
@@ -48,24 +113,40 @@ const SignUp = () => {
                             <Box>
                                 <FormControl isRequired>
                                     <FormLabel>Full Name</FormLabel>
-                                    <Input type="text" />
+                                    <Input type="text"
+
+                                        onChange={(e) => setInputs({ ...inputs, name: e.target.value })}
+
+                                        value={inputs.name} />
                                 </FormControl>
                             </Box>
                             <Box>
                                 <FormControl isRequired >
                                     <FormLabel>Username</FormLabel>
-                                    <Input type="text" />
+                                    <Input type="text"
+
+                                        onChange={(e) => setInputs({ ...inputs, username: e.target.value })}
+
+                                        value={inputs.username} />
                                 </FormControl>
                             </Box>
                         </HStack>
                         <FormControl isRequired>
                             <FormLabel>Email address</FormLabel>
-                            <Input type="email" />
+                            <Input type="email"
+
+                                onChange={(e) => setInputs({ ...inputs, email: e.target.value })}
+
+                                value={inputs.email} />
                         </FormControl>
                         <FormControl isRequired>
                             <FormLabel>Password</FormLabel>
                             <InputGroup>
-                                <Input type={showPassword ? 'text' : 'password'} />
+                                <Input type={showPassword ? 'text' : 'password'}
+
+                                    onChange={(e) => setInputs({ ...inputs, password: e.target.value })}
+                                    
+                                    value={inputs.password} />
                                 <InputRightElement h={'full'}>
                                     <Button
                                         variant={'ghost'}
@@ -83,13 +164,14 @@ const SignUp = () => {
                                 color={'white'}
                                 _hover={{
                                     bg: useColorModeValue('gray.700', 'gray.800')
-                                }}>
+                                }}
+                                onClick={handleSignup}>
                                 Sign up
                             </Button>
                         </Stack>
                         <Stack pt={6}>
                             <Text align={'center'}>
-                                Already a user? <Link onClick={() => {setAuthScreen("login")}} color={'blue.400'}>Login</Link>
+                                Already a user? <Link onClick={() => { setAuthScreen("login") }} color={'blue.400'}>Login</Link>
                             </Text>
                         </Stack>
                     </Stack>
